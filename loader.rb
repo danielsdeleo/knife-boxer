3bb0b2d5a771525edb0bbf35ce3d2f0b56439de6
danielsdeleo <dan@opscode.com>
2013-02-09 13:43:15 -0800

    update loader so lib files load correctly

--- loader.rb
+++ loader.rb
@@ -2,5 +2,6 @@
 # knife-boxer from a git checkout without stomping on your
 # ~/.chef/plugins/knife directory.
 
+$:.unshift(File.expand_path("../lib", __FILE__))
 glob = File.expand_path("../lib/chef/knife/*rb",__FILE__)
 Dir[glob].each {|f| load(f) }
