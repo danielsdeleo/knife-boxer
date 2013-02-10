26ed89e95e51e49f39bec2ffede9b90460882209
danielsdeleo <dan@opscode.com>
2013-02-09 16:11:14 -0800

    log to pager (less). No config to turn on/off ATM.

--- lib/chef/knife/log.rb
+++ lib/chef/knife/log.rb
@@ -9,10 +9,31 @@ module KnifeBoxer
     def run
       all_entries = Chef::Search::Query.new.search("cookbook-up-log")[0]
       log_entries = all_entries.sort_by {|e| e["id"] }.reverse
-      log_entries.each {|e| display_entry(e) }
+      pager_pid, write = setup_pager
+      log_entries.each {|e| display_entry(write, e) }
+      write.close
+      Process.wait(pager_pid)
     end
 
-    def display_entry(entry)
+    def setup_pager
+      read, write = IO.pipe
+      write.sync = true
+      read.sync = true
+      pager_pid = fork do
+        write.close
+        STDIN.reopen(read)
+        IO.select([read], nil, nil, 5)
+        exec("less")
+      end
+      read.close
+      [pager_pid, write]
+    end
+
+    def display_entry(io, entry)
+      io.print(format_entry(entry))
+    end
+
+    def format_entry(entry)
       message=<<-E
 Environment: #{entry["environment"]}
   Updated by: #{entry["user"]}
@@ -27,7 +48,6 @@ E
         message << "  #{update.description(justify_width)}\n"
       end
       message << "\n"
-      ui.info message
     end
 
   end
